417b6e91515c17b418b2ee4928ba1b075b1f2d64
zooozoo <shz0309@gmail.com>
2017-09-28 16:27:53 +0900

    str 메서드 재수정

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -13,4 +13,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return f'{self.title} (설문:{self.question.title}'
+        return f'{self.title} (설문:{self.question.title})'
