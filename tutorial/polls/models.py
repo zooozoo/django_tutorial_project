4337a0fadbfbc96faa332b642cae6a2009d70591
zooozoo <shz0309@gmail.com>
2017-09-28 16:26:37 +0900

    str메서드 수정

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -5,9 +5,12 @@ class Question(models.Model):
     published_date = models.DateTimeField(blank=True)
 
     def __str__(self):
-        return self.title
+        return f'설문조사 ({self.title})'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     title = models.CharField(max_length=50)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.title} (설문:{self.question.title}'
